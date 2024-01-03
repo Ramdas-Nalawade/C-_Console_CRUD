2b1415e1c01313902b4390698cb392135867ed0b
Ramdas-Nalawade <147508047+Ramdas-Nalawade@users.noreply.github.com>
2024-01-02 18:24:38 -0800

    Add files via upload

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EMPLOYEE_MANAGEMENT.Entities;
+using EMPLOYEE_MANAGEMENT.Services;
+
+namespace EMPLOYEE_MANAGEMENT.Controllers
+{
+    public class EmployeeController
+    {
+        private IEmployeeService empService;
+
+        public EmployeeController(IEmployeeService empService)
+        {
+            this.empService = empService;
+        }
+        public List<Employee> GetAll()
+        {
+            return empService.GetAll();
+        }
+    }
+}
