4ecb3282bf4205560b9a25fbbf09b74c3a0cf374
Ramdas-Nalawade <147508047+Ramdas-Nalawade@users.noreply.github.com>
2024-01-02 18:23:45 -0800

    Add files via upload

---
+++ Services/EmployeeService.cs
@@ -0,0 +1,19 @@
+﻿using EMPLOYEE_MANAGEMENT.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMPLOYEE_MANAGEMENT.Services
+{
+    public class EmployeeService : IEmployeeService
+    {
+        public List<Employee> GetAll()
+        {
+            List<Employee> list = new List<Employee>();
+            list.Add(new Employee { Id = 1, Name = "Shraddha" });
+            return list;
+        }
+    }
+}
---
+++ Services/IEmployeeService.cs
@@ -0,0 +1,14 @@
+﻿using EMPLOYEE_MANAGEMENT.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMPLOYEE_MANAGEMENT.Services
+{
+    public interface IEmployeeService
+    {
+        List<Employee> GetAll();
+    }
+}
